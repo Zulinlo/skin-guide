4e76399d90becec626766a97c3dfe2b054521ebd
Zulinlo <samm.wu2@gmail.com>
2021-04-12 21:41:53 +1000

    Home linked with components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,12 @@
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import Steps from '../Steps';
+
 const Home = () => {
+
     return (
         <>
+            <Navbar />
             <h1>Home</h1>
         </>
     );
